a61b1fd69371a254fd810564f3c74fa22b9786d5
kumastry <chma19063@g.nihon-u.ac.jp>
2023-03-17 02:46:52 +0900

    404ページのルーティングを忘れたので、その修正を行った

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Top } from "./pages/Top";
+import { NotFound } from "./pages/NotFound";
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={`/`} element={<Top />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );

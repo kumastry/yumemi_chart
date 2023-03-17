4a9df545ddaca50f490f3a30ff6afb5b95c4aaaf
kumastry <chma19063@g.nihon-u.ac.jp>
2023-03-17 13:58:34 +0900

    react-queryのqueryClientとproviderの設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,29 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Top } from "./pages/Top";
 import { NotFound } from "./pages/NotFound";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+      staleTime: 60000,
+      cacheTime: 60000,
+    },
+  },
+});
 
 function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={`/`} element={<Top />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={`/`} element={<Top />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 

c9237983b89dfe1b14db17b00ea091c0c454f89f
kumastry <chma19063@g.nihon-u.ac.jp>
2023-03-17 15:37:47 +0900

    fetchした都道府県をチェックボックスで表示

--- src/pages/Top/Top.tsx
+++ src/pages/Top/Top.tsx
@@ -1,5 +1,38 @@
 import React from "react";
+import { useFetchPrefectures } from '../../hooks/useFetchPrefectures';
 
 export const Top = (): JSX.Element => {
-  return <>トップ画面</>;
+
+  const {status, data} = useFetchPrefectures();
+  const [prefectPopulation, setPrefectPopulation] = React.useState({});
+
+  const handleChange = (e : any) => {
+    console.log(e.target?.id);
+    console.log(e.target?.value);
+  }
+  if(status === "loading") {
+    return <div>Loading...</div>
+  }
+
+  if(status === "error") {
+    return <div>Error</div>;
+  }
+
+  return(
+    <>
+    <form method = "get">
+    <ul>
+      {data?.result.map((prefecture) => {
+        
+        return (
+          <li>
+               <input type="checkbox" id={String(prefecture.prefCode)}  name={prefecture.prefName}  onChange={handleChange}/>
+               <label htmlFor={String(prefecture.prefCode)}>{prefecture.prefName}</label>
+          </li>
+        );
+      })}
+    </ul>
+    </form>
+    </>
+  );
 };

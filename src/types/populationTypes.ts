5312699528894d974424f7caa0e0d6b26c4a5476
kumastry <chma19063@g.nihon-u.ac.jp>
2023-03-19 17:41:17 +0900

    interface PrefectureとPrefecturesをPopulationTypesに移動した

--- src/types/populationTypes.ts
+++ src/types/populationTypes.ts
@@ -16,3 +16,13 @@ export type PrefecturePopulationByYearWithType = {
 export type PrefecturePopulationWithIdx = {
   [key in PopulationCompositionType]: 0 | 1 | 2 | 3;
 };
+
+export interface Prefecture {
+  prefCode: number;
+  prefName: string;
+}
+
+export interface Prefectures {
+  message: string | null;
+  result: Prefecture[];
+}
